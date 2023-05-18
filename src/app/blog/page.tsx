589f4e3c9bff9a8ef9a3f167fb779e14fcc5b867
Mai Xu <maixu@Mais-MacBook-Pro.local>
2023-05-18 12:10:48 -0400

    Update homepage and blogpage

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+const BlogNavigation = () => {
+    // Dummy data for blogs (replace it with your actual data)
+    const blogs = [
+        {
+            id: 1,
+            title: "Blog 1",
+            date: "May 10, 2023",
+            image: "/images/blog/blog1.jpg",
+        },
+        {
+            id: 2,
+            title: "Blog 2",
+            date: "May 12, 2023",
+            image: "/images/blog/blog2.jpg",
+        },
+        {
+            id: 3,
+            title: "Blog 3",
+            date: "May 15, 2023",
+            image: "/images/blog/blog3.jpg",
+        },
+    ];
+
+    return (
+        <Layout>
+            <main
+                className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+                <div className="container mx-auto bg-white">
+                    <h2 className="text-2xl font-bold mb-4 flex items-center justify-center w-full px-4 mx-auto max-w-7xl sm:px-6">
+                        Latest Blogs
+                    </h2>
+                    <div className="grid gap-4">
+                        {blogs.map((blog) => (
+                            <div
+                                key={blog.id}
+                                className="border border-gray-200 rounded p-4 hover:shadow-md transition duration-300"
+                            >
+                                <img
+                                    src={blog.image}
+                                    alt={blog.title}
+                                    className="w-full h-40 object-cover rounded mb-2"
+                                />
+                                <h3 className="text-lg font-bold mb-1">{blog.title}</h3>
+                                <p className="text-gray-500 text-sm">{blog.date}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </main>
+        </Layout>
+    );
+};
+
+
+export default BlogNavigation;
---
+++ src/components/layout.tsx
@@ -0,0 +1,11 @@
+import React, { PropsWithChildren } from "react";
+import Navbar from "./navbar";
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <Navbar />
+      {children}
+    </>
+  );
+};
+export default Layout;
